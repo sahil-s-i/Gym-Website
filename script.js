b73016d73cec7ae812fb66b5a1349e35a506dec2
Sahil <sahilismailnavar@gmail.com>
2023-08-14 10:08:20 +0530

    Added script file to the website

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    console.log("open funciton")
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    console.log("close funciton")
+    document.getElementById("mySidenav").style.width = "0";
+}
